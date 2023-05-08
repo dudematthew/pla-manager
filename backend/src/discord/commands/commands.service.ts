8f847fd7851022709435fafe312dbff3cd4723d8
dudematthew <mmoczydlowski4@gmail.com>
2023-05-08 10:44:10 +0200

    Improve config module, improve getRoles command

--- backend/src/discord/commands/commands.service.ts
+++ backend/src/discord/commands/commands.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { Context, SlashCommand, SlashCommandContext } from 'necord';
 import { RoleService } from 'src/database/entities/role/role.service';
 import { EmbedBuilder } from 'discord.js';
+import { ConfigService } from '@nestjs/config';
+// import * as paginationEmbed from 'discord.js-pagination';
 
 @Injectable()
 export class CommandsService {
 
     constructor(
         private readonly roleService: RoleService,
+        private readonly configService: ConfigService,
     ) {}
     
     /**
@@ -32,22 +35,62 @@ export class CommandsService {
     })
     public async onGetRoles(@Context() [Interaction]: SlashCommandContext) {
         const roles = await this.roleService.findAll();
-        const roleEmbed = new EmbedBuilder()
-            .setTitle('Lista ról')
-            .setColor('#0099ff')
-            .setTimestamp()
-            .setAuthor({
-                name: 'Polskie Legendy Apex',
-                iconURL: 'https://i.imgur.com/u4DEkaz.png'
-            });
 
-        roles.forEach(role => {
-            roleEmbed.addFields({
-                name: role.name,
-                value: `<@&${role.discordId}> - ${role.discordId}`,
-            });
-        });
+        const pages = [];
 
-        return Interaction.reply({ embeds: [roleEmbed], ephemeral: true});
+        // let page = 1;
+        // roles.forEach(role => {
+        //     let roleEmbed: EmbedBuilder;
+            
+        //     // Every 10 roles, create a new page
+        //     if (page % 10 === 0 || page === 1) {
+        //         roleEmbed = new EmbedBuilder()
+        //             .setTitle('Lista ról')
+        //             .setColor(this.configService.get('theme.color-primary'))
+        //             .setTimestamp()
+        //             .setAuthor({
+        //                 name: 'Polskie Legendy Apex',
+        //                 iconURL: this.configService.get('images.logo')
+        //             });
+        //     }
+
+        //     roleEmbed.addFields({
+        //         name: role.name,
+        //         value: `<@&${role.discordId}> - ${role.discordId}`,
+        //     });
+
+        //     // Every 10 roles, push the page to the pages array
+        //     if (page % 10 === 0) {
+        //         pages.push(roleEmbed);
+        //     }
+        // });
+
+        // // For loop variation
+        // for (let i = 0; i < roles.length; i++) {
+        //     let roleEmbed: EmbedBuilder;
+
+        //     // Every 10 roles, create a new page
+        //     if (i % 2 === 0 || i === 1) {
+        //         roleEmbed = new EmbedBuilder()
+        //         .setTitle('Lista ról')
+        //             .setColor(this.configService.get('theme.color-primary'))
+        //             .setTimestamp()
+        //             .setAuthor({
+        //                 name: 'Polskie Legendy Apex',
+        //                 iconURL: this.configService.get('images.logo')
+        //             });
+
+        //         pages.push(roleEmbed);
+        //     }
+
+        //     pages[pages.length - 1].addFields({
+        //         name: roles[i].name,
+        //         value: `<@&${roles[i].discordId}> - ${roles[i].discordId}`,
+        //     });
+        // }
+
+        // const embed = paginationEmbed(Interaction, pages, ['⏪', '⏩'], 30000);
+        
+        // return Interaction.reply({ embeds: [roleEmbed], ephemeral: true});
     }
 }
