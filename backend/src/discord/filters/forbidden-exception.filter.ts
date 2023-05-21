a6a2b5b9b4f82a974eb30ffe18105dd018f1b601
dudematthew <mmoczydlowski4@gmail.com>
2023-05-21 19:02:10 +0200

    Fix exception filter throwing an error

--- backend/src/discord/filters/forbidden-exception.filter.ts
+++ backend/src/discord/filters/forbidden-exception.filter.ts
@@ -1,13 +1,15 @@
 import { EmbedBuilder } from "@discordjs/builders";
-import { ArgumentsHost, Catch, ExceptionFilter, ForbiddenException, Logger } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, ForbiddenException, Inject, Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { SlashCommandContext } from "necord";
+import { RGBTuple } from "discord.js";
 
 @Catch(ForbiddenException)
 export class ForbiddenExceptionFilter implements ExceptionFilter {
     private readonly logger = new Logger(ForbiddenExceptionFilter.name);
 
     constructor(
+        @Inject(ConfigService)
         private readonly configService: ConfigService,
     ) {}
 
@@ -16,20 +18,27 @@ export class ForbiddenExceptionFilter implements ExceptionFilter {
 
         // Build nice embed message for the user to see
 
-        const embed = new EmbedBuilder()
-            .setTitle('❌ Brak uprawnień!')
-            .setDescription('Nie masz uprawnień do wykonania tej komendy!')
-            .setColor(this.configService.get('color-primary'))
-            .setTimestamp()
-            .setAuthor({
-                name: 'Polskie Legendy Apex',
-                iconURL: this.configService.get('logo'),
-            })
-            
-        await interaction.reply({
-            embeds: [ embed ],
-            ephemeral: true,
-        });
+        try {
+            const embed = new EmbedBuilder()
+                .setTitle('❌ Brak uprawnień!')
+                .setDescription('Nie masz uprawnień do wykonania tej komendy!')
+                // .setColor(this.configService.get<RGBTuple>('theme.color-primary'))
+                .setTimestamp()
+                .setAuthor({
+                    name: 'Polskie Legendy Apex',
+                    iconURL: this.configService.get('images.logo')
+                });
+
+                await interaction.reply({
+                    embeds: [ embed ],
+                    ephemeral: true,
+                });
+        } catch (e) {
+            this.logger.error(`Nie udało się stworzyć wiadomości: ${e}`);
+            console.log(this.configService.get('theme.color-primary'), this.configService.get('images.logo'), e);
+        }
+
+
 
         this.logger.error(`Użytkownik ${interaction.user.username} (${interaction.user.id}) nie ma uprawnień do wykonania komendy ${interaction.commandName}!`)
     }
