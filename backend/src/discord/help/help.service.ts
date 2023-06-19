4fa11e3a8d5e3b83caff792398b5e2571dd157e6
dudematthew <mmoczydlowski4@gmail.com>
2023-06-19 16:31:30 +0200

    Improve help service code, create basic cmd tree

--- backend/src/discord/help/help.service.ts
+++ backend/src/discord/help/help.service.ts
@@ -3,6 +3,13 @@ import { DiscordService } from '../discord.service';
 import { ApplicationCommand, ApplicationCommandOptionData, ApplicationCommandOptionType, ApplicationCommandType, CacheType, ChatInputCommandInteraction } from 'discord.js';
 import { MenuOption, Row, RowTypes } from 'discord.js-menu-buttons';
 
+interface ICommandGroup {
+    name: string;
+    description: string;
+    id: string;
+    commands: ICommand[];
+}
+
 interface ICommand {
     name: string;
     description: string;
@@ -88,44 +95,85 @@ export class HelpService {
         const discordCommands = await this.discordService.getApplicationCommands();
         const commands = [];
 
-        // for (const command of discordCommands.values()) {
-        //     const options = command.options;
-        //     let subCommands = [];
+        const iCommands = await this.createICommandsGroups(discordCommands);
 
-        //     if (typeof command.options !== 'undefined') {
-        //         for (const option of options) {
-        //             const iOption: ICommandOption = {
-        //                 name: option.name,
-        //                 description: option.description,
-        //                 type: option.type,
-        //             };
+        console.log(iCommands);
 
-        //             if (option.type !== ApplicationCommandOptionType.Subcommand &&
-        //                 option.type !== ApplicationCommandOptionType.SubcommandGroup
-        //             ) {
-        //                 commands.push();
-
-        //                 continue;
-        //             }
-
-        //             const iSubCommand = this.createICommand(option);
-
-
-        //         }
-
-        //         iCommand.options = subCommands;
-        //     }
-        // }
-
-
+        return iCommands;
     }
 
-    // private async createICommand(command): Promise<ICommand> {
-    //     const iCommand: ICommand = {
-    //         name: command.name,
-    //         description: command.description,
-    //     }        
+    private async createICommandsGroups(commands) : Promise<{ [key: string]: ICommand[] }> {
+        const iCommandsGroups = {
+            default: [],
+        }
 
-    //     return iCommand;
-    // }
+        for (const command of commands.values()) {
+            // Check if command has options
+            if (typeof command.options !== 'undefined' && command.options.length > 0) {
+                
+                    // Check if command has options
+                    // (aLl options are either subcommand or just command option)
+                    if (command.options[0].type === ApplicationCommandOptionType.Subcommand ||
+                        command.options[0].type === ApplicationCommandOptionType.SubcommandGroup
+                    ) {
+                        iCommandsGroups[command.name] = await this.createICommandsGroup(command.options);
+    
+                        continue;
+                    }
+                    else {
+                        iCommandsGroups['default'].push(await this.createICommand(command));
+                    }
+            } else
+                iCommandsGroups['default'].push(await this.createICommand(command));
+        }
+        
+
+        return iCommandsGroups;
+    }
+
+    /**
+     * Create a group of commands
+     * Accepts only subcommands and subcommand groups
+     * @param commands 
+     * @param commandsGroupName 
+     */
+    private async createICommandsGroup(commands): Promise<ICommand[]> {
+        const iCommandGroup = [];
+
+        for (const command of commands.values()) {
+            iCommandGroup.push(await this.createICommand(command));
+        }
+
+        return iCommandGroup;
+    }
+
+    /**
+     * Create ICommand from ApplicationCommand ONLY if it's NOT 
+     * a subcommand or subcommand group
+     * @param command 
+     */
+    private async createICommand(command): Promise<ICommand> {
+        const iCommand: ICommand = {
+            name: command.name,
+            description: command.description,
+        };
+
+        if (typeof command.options !== 'undefined') {
+            const options = [];
+
+            for (const option of command.options) {
+                const iOption: ICommandOption = {
+                    name: option.name,
+                    description: option.description,
+                    type: option.type,
+                };
+
+                options.push(iOption);
+            }
+
+            iCommand.options = options;
+        }
+
+        return iCommand;
+    }
 }
