ce3ea6cd4b75ec5dfca799685126ce477ea620cf
dudematthew <mmoczydlowski4@gmail.com>
2023-08-01 16:34:46 +0200

    Fix .nix file and remove

--- shell.nix
+++ shell.nix
@@ -1,14 +1,9 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodejs-19_1_0
-    pkgs.libuuid
-  ];
-  env = { 
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid]; 
-    NODE_ENV = "production";
-  }; 
-  shellHook = ''
-    echo "The .nix file is being used."
-    npm run deploy
-  '';
-}
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    buildInputs = [
+      pkgs.nodejs-16_x
+      pkgs.util-linux
+    ];
+  }
